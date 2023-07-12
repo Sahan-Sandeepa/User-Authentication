31072dc9de5b5425230cd30f6f843c0949918f21
Sahan-Sandeepa <sahansandeepa0003@gmail.com>
2023-07-12 20:12:29 +0530

    initial commit

---
+++ src/main/java/dev/sahan/userverification/UserverificationApplication.java
@@ -0,0 +1,13 @@
+package dev.sahan.userverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserverificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserverificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sahan/userverification/UserverificationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sahan.userverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserverificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
